9ef4b56378fd23e934a607c244627d744ba243b6
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-01-21 00:09:08 +0300

    working on popup

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let shareButtons = document.querySelectorAll('.share');
 let contactButton = document.querySelector('.contact-btn');
 let formPopUp = document.querySelector('.form-popup'); 
 let exitButton = document.querySelector('.exit'); 
+let popUpInputs = document.querySelectorAll('.input-border');
+let popUpBtn = document.querySelector('.popup-contact-btn');
+
 let url = '';
 
 shareButtons.forEach((shareButton) => {
@@ -12,10 +15,15 @@ shareButtons.forEach((shareButton) => {
 });
 
 contactButton.onclick = () => {
-
   formPopUp.style.display = 'block';
 }
 
 exitButton.onclick = () => {
   formPopUp.style.display = 'none';
+}
+
+popUpBtn.onclick = () =>{
+  popUpInputs.forEach(input => {
+    console.log(input.value);
+  });
 }
