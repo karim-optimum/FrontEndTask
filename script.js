2bd73db5b8a91290c7af843fe31a27ca116ad934
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-01-21 16:16:27 +0300

    fix

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let formPopUp = document.querySelector('.form-popup');
 let exitButton = document.querySelector('.exit'); 
 let popUpInputs = document.querySelectorAll('.input-border');
 let popUpBtn = document.querySelector('.popup-contact-btn');
+let hamburgerMenu = document.querySelector('.hamburger');
+let navMenu = document.querySelector('.nav-menu');
+let links = document.querySelectorAll('.nav-link');
+
+links.forEach(n => n.onclick = () => {
+  hamburgerMenu.classList.remove('active')
+  navMenu.classList.remove('active')
+})
 
 let url = '';
 
@@ -27,4 +35,9 @@ popUpBtn.onclick = () =>{
   popUpInputs.forEach(input => {
     console.log(input.value);
   });
+}
+
+hamburgerMenu.onclick = ()=> {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
