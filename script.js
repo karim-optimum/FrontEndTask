b54cd24ba18f6a5712f4426c4cc9091b20f90196
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-01-22 15:56:12 +0300

    working on responsivness

--- script.js
+++ script.js
@@ -7,12 +7,9 @@ let popUpBtn = document.querySelector('.popup-contact-btn');
 let hamburgerMenu = document.querySelector('.hamburger');
 let navMenu = document.querySelector('.nav-menu');
 let links = document.querySelectorAll('.nav-link');
-
-links.forEach(n => n.onclick = () => {
-  hamburgerMenu.classList.remove('active')
-  navMenu.classList.remove('active')
-})
-
+const cards = document.querySelectorAll('.card');
+let prevButton = document.getElementById('prev');
+let nextButton = document.getElementById('next');
 let url = '';
 
 shareButtons.forEach((shareButton) => {
@@ -22,10 +19,15 @@ shareButtons.forEach((shareButton) => {
   };
 });
 
+links.forEach(link => link.onclick = () => {
+  hamburgerMenu.classList.remove('active')
+  navMenu.classList.remove('active')
+});
+
 contactButton.onclick = () => {
   formPopUp.classList.remove('form-display');
   formPopUp.style.display = 'block';
-}
+};
 
 exitButton.onclick = () => {
   formPopUp.classList.add('form-display');
@@ -35,43 +37,51 @@ popUpBtn.onclick = () =>{
   popUpInputs.forEach(input => {
     console.log(input.value);
   });
-}
+};
 
 hamburgerMenu.onclick = ()=> {
   hamburgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
-}
+};
 
 function closePopup() {
   // Code to close the popup
   formPopUp.style.display = 'none';
-}
+};
 
-// Add a click event listener to the document
-document.addEventListener('click', function(event) {
+document.onclick =  (event) => {
   if (formPopUp.contains(event.target)) {
     closePopup();
-  }
-});
-
-
+}};
 
 let currentIndex = 0;
-const cards = document.querySelectorAll('.card');
 
-document.getElementById('prev').addEventListener('click', () => {
+function updateButtonState() {
+ if (window.matchMedia("(min-width: 1100px)").matches) {
+    // If viewport is wider than 1200px, disable the buttons
+    prevButton.disabled = true;
+    nextButton.disabled = true;
+ } else {
+    // Otherwise, enable the buttons
+    prevButton.disabled = false;
+    nextButton.disabled = false;
+ }
+};
+
+updateButtonState();
+
+prevButton.onclick = () => {
  if(currentIndex > 0){
     cards[currentIndex].style.display = 'none';
     currentIndex--;
     cards[currentIndex].style.display = 'block';
  }
-});
+};
 
-
-document.getElementById('next').addEventListener('click', () => {
+nextButton.onclick = () => {
  if(currentIndex < cards.length - 1){
     cards[currentIndex].style.display = 'none';
     currentIndex++;
     cards[currentIndex].style.display = 'block';
  }
-});
+};
